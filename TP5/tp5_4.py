dda52220cbd689a00473dee33cfe1a66a40c8752
Rapha <raphanicaise@gmail.com>
2024-09-12 10:45:03 -0300

    Punto 4

--- TP5/tp5_4.py
+++ TP5/tp5_4.py
@@ -1,1 +1,25 @@
-#
+from tp5_3 import Color
+
+class testColores:
+    @staticmethod
+    def test():
+    #a
+        color_1 = Color() 
+        color_2 = Color(70, 70, 70) 
+        color_3 = Color(255, 255, 255) 
+        igualdad1 = color_1.esIgualQue(color_2) 
+        igualdad2 = color_2.esIgualQue(color_3) 
+        color_4 = color_1 
+        color_5 = color_2.clonar()
+        # b
+        color_6 = Color(140,250,140) 
+        color_4 = color_6 
+        color_2 = color_5.clone() 
+        igualdad3 = color_2.esIgualQue(color_5) 
+        color_3 = color_2 
+        color_1 = color_5.complemento() 
+        # c 
+        color_1 = color_5.complemento() 
+        color_2 = color_5.clone() 
+        color_3 = color_2 
+ 
