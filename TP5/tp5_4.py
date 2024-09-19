e876be89bf0040169d9a699f14b7e71c6df1f9ce
Rapha <raphanicaise@gmail.com>
2024-09-19 08:49:44 -0300

    Testeo de datos

--- TP5/tp5_4.py
+++ TP5/tp5_4.py
@@ -5,21 +5,35 @@ class testColores:
     def test():
     #a
         color_1 = Color() 
-        color_2 = Color(70, 70, 70) 
-        color_3 = Color(255, 255, 255) 
-        igualdad1 = color_1.esIgualQue(color_2) 
-        igualdad2 = color_2.esIgualQue(color_3) 
-        color_4 = color_1 
+        print(color_1)
+        color_2 = Color(70, 70, 70)
+        print(color_2) 
+        color_3 = Color(255, 255, 255)
+        print(color_3)  
+        print(color_1.esIgualQue(color_2))
+        print(color_2.esIgualQue(color_3)) 
+        color_4 = color_1
+        print(color_4) 
         color_5 = color_2.clonar()
+        print(color_5)
         # b
-        color_6 = Color(140,250,140) 
-        color_4 = color_6 
-        color_2 = color_5.clone() 
-        igualdad3 = color_2.esIgualQue(color_5) 
-        color_3 = color_2 
-        color_1 = color_5.complemento() 
+        color_6 = Color(140,250,140)
+        print(color_6) 
+        color_4 = color_6
+        print(color_4) 
+        color_2 = color_5.clonar()
+        print(color_2) 
+        print(color_2.esIgualQue(color_5)) 
+        color_3 = color_2
+        print(color_3) 
+        color_1 = color_5.complemento()
+        print(color_1) 
         # c 
         color_1 = color_5.complemento() 
-        color_2 = color_5.clone() 
-        color_3 = color_2 
+        color_2 = color_5.clonar()
+        print(color_2)  
+        color_3 = color_2
+        print(color_3) 
  
+if __name__ == '__main__':
+    testColores.test()
